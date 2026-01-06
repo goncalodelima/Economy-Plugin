e9c18921c3e67d42d10c0f2bc8bbb7f40f51cbd8
Gonçalo <gongaslima2007@gmail.com>
2026-01-06 14:19:13 +0000

    API Modifications (1.0.4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
 
 allprojects {
     group = "pt.gongas"
-    version = "1.0.3"
+    version = "1.0.4"
     description = "Economy plugin system"
     ext.set("id", "economyplugin")
     ext.set("website", "https://github.com/goncalodelima/Economy-Plugin")
