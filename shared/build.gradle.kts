6bf2ac8322255fe2a87f12698d33d73e257b54c1
Gonçalo <gongaslima2007@gmail.com>
2026-03-07 11:25:25 +0000

    Replaced fixed thread pool with virtual threads executor

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
 }
 
 group = "pt.gongas"
-version = "1.2.5"
+version = "1.2.6"
 description = "Shared module for Economy Plugin"
 
 dependencies {
--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -45,7 +45,7 @@ java {
 }
 
 group = "pt.gongas"
-version = "1.2.5"
+version = "1.2.6"
 description = "Velocity module for Economy Plugin"
 
 dependencies {
